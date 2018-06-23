348c3e5b43977dacd7dec58d3d4424f640d28e4a
LorakPL <karolfur1995@gmail.com>
2018-06-23 12:27:12 +0200

    initial commit

---
+++ src/main/java/com/filmoteka/v2/rest/FilmotekaV2RestApplication.java
@@ -0,0 +1,12 @@
+package com.filmoteka.v2.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmotekaV2RestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmotekaV2RestApplication.class, args);
+    }
+}
